fcea5dfaa887691997d9943396caec6a44a3e9ff
RogueMacro <william.tetlie@gmail.com>
2026-01-25 01:13:36 +0100

    x86

--- main.s
+++ main.s
@@ -1,26 +1,17 @@
-//
-// Assembler program to print "Hello World!"
-// to stdout.
-//
-// X0-X2 - parameters to Unix system calls
-// X16 - Mach System Call function number
-//
+section .data
+msg db "hello world\n"
+len equ $ - msg
 
-.global _main   // Provide program starting address to linker
-.align 2   // Make sure everything is aligned properly
+section .text
+global _start
 
-// Setup the parameters to print hello world
-// and then call the Kernel to do it.
-_main: mov X0, #1  // 1 = StdOut
- adr X1, helloworld  // string to print
- mov X2, #14      // length of our string
- mov X16, #4  // Unix write system call
- svc #0x80  // Call kernel to output the string
+_start:
+    mov edx, len
+    mov ecx, msg
+    mov ebx, 1
+    mov eax, 4
+    int 80h
 
-// Setup the parameters to exit the program
-// and then call the kernel to do it.
- mov     X0, #0  // Use 0 return code
- mov     X16, #1  // System call number 1 terminates this program
- svc     #0x80  // Call kernel to terminate the program
-
-helloworld:      .ascii  "Hello, World!\n"
+    mov ebx, 0
+    mov eax, 1
+    int 80h
